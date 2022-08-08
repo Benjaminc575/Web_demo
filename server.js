758439eeb234c9c1f9836f8867c007a0c99ac799
Benjamin Cardenas <benjaminc5751@gmail.com>
2022-08-08 15:54:08 -0500

    message

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const mongo = require('/mongo');
+const mongo = require('./mongo');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
