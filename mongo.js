4809a881423680740bb814a2ebf35d3a8076db89
Benjamin Cardenas <benjaminc5751@gmail.com>
2022-08-09 21:10:48 -0500

    mongodb issue

---
+++ mongo.js
@@ -0,0 +1,7 @@
+const mongo = 'mongodb+srv://FilmRev22:BxgSATCBxgs8X3fM@cluster0.pszru.mongodb.net/?retryWrites=true&w=majority'
+//BxgSATCBxgs8X3fM
+
+module.exports = mongo;
+
+//username:     FilmRev22
+//pass:         BxgSATCBxgs8X3fM
