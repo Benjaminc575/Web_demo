4dd096ee63f64a7c839d62065edc040e9431d207
Benjamin Cardenas <92549774+Benjaminc575@users.noreply.github.com>
2022-08-09 21:20:46 -0500

    Delete mongo.js

--- mongo.js
+++
@@ -1,7 +0,0 @@
-const mongo = 'mongodb+srv://FilmRev22:BxgSATCBxgs8X3fM@cluster0.pszru.mongodb.net/?retryWrites=true&w=majority'
-//BxgSATCBxgs8X3fM
-
-module.exports = mongo;
-
-//username:     FilmRev22
-//pass:         BxgSATCBxgs8X3fM
