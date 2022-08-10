ea6e1bd73c4a67ff2be063ee44529eec501abcaa
Benjamin Cardenas <benjaminc5751@gmail.com>
2022-08-10 12:28:45 -0500

    message

--- server.js
+++ server.js
@@ -5,7 +5,12 @@ const mongoose = require('mongoose');
 const route = require('./route/pages');
 const path = require('path');
 const app = express();
-const uri = process.env.MONGODB_URI;
+//const uri = process.env.MONGODB_URI;
+let port = process.env.PORT;
+
+if (port == null || port == ""){
+    port = 3000;
+}
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
@@ -14,8 +19,8 @@ app.use('/', route);
 
 mongoose.connect(mongo, {useNewURLParser: true, useUnifiedTopology: true})
 .then(() => {
-    app.listen(process.env.PORT||3000, ()=> {
-        console.log("Server is running on 3000")
+    app.listen(port, ()=> {
+        console.log('Listening on port ${port}')
     })
 })
 
